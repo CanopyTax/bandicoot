707d3eb200eebf399df3f16d9164903157030004
Devin Rasmussen <devin.rasmussen@canopytax.com>
2019-02-27 12:15:58 -0700

    update to use useDocumentExecCommand hook and better nomenclature

---
+++ src/use-format-block.hook.js
@@ -0,0 +1,16 @@
+import {useContext} from 'react'
+import {useDocumentExecCommand} from './use-document-exec-command.hook'
+
+export function useFormatBlock() {
+  const {performCommandWithValue} = useDocumentExecCommand('formatBlock')
+
+  return {
+    formatBlock(value) {
+      if (value === document.queryCommandValue('formatBlock')) {
+        performCommandWithValue('div')
+      } else {
+        performCommandWithValue(value)
+      }
+    },
+  }
+}

65fdaa10f0d737eb5fcc747675662f90d55d72b4
Devin Rasmussen <devin.rasmussen@canopytax.com>
2019-07-17 11:23:33 -0600

    check if chrome and manually remove header from DOM if in a list

--- src/use-format-block.hook.js
+++ src/use-format-block.hook.js
@@ -5,11 +5,47 @@ export function useFormatBlock() {
 
   return {
     formatBlock(value) {
+      console.log(value, document.queryCommandValue('formatBlock'));
       if (value === document.queryCommandValue('formatBlock')) {
-        performCommandWithValue('div')
+        if (!!window.chrome && isInHTMLList()) {
+          removeHeader()
+        }
+        else {
+          performCommandWithValue('div')
+        }
       } else {
         performCommandWithValue(value)
       }
     },
   }
+}
+
+function isInHTMLList() {
+  let containerNode = window.getSelection().getRangeAt(0).commonAncestorContainer
+  let index = 0
+  let isList = false
+  console.log(containerNode.parentNode)
+  while(isList == false && index < 2) {
+    if (containerNode.nodeType === Node.ELEMENT_NODE && containerNode.tagName === 'LI') {
+      return true
+    }
+    containerNode = containerNode.parentNode
+    index++
+  }
+}
+
+function removeHeader() {
+  let nodeToBeRemoved = window.getSelection().getRangeAt(0).commonAncestorContainer
+  let index = 0
+  while (nodeToBeRemoved.tagName !== 'H1' && index < 5) {
+    nodeToBeRemoved = nodeToBeRemoved.parentNode
+    index ++
+  }
+  if (index === 4) {
+    throw new Error(`The element does not appear to be wrapped by a header`)
+  }
+  while (nodeToBeRemoved.firstChild) {
+    nodeToBeRemoved.parentNode.insertBefore(nodeToBeRemoved.firstChild, nodeToBeRemoved);
+  }
+  nodeToBeRemoved.parentNode.removeChild(nodeToBeRemoved);
 }

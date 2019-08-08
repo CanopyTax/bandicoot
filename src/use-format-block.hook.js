6cb6943763aa8fc6421d44627d8766deb07c31c4
Devin Rasmussen <devin.rasmussen@canopytax.com>
2019-08-08 16:41:32 -0600

    create constant 'HEADER_TAGS' to be reused, throw error if no header is found in removeHeader

--- src/use-format-block.hook.js
+++ src/use-format-block.hook.js
@@ -1,12 +1,14 @@
 import {useDocumentExecCommand} from './use-document-exec-command.hook'
 
+const HEADER_TAGS = ['H1', 'H2', 'H3', 'H4', 'H5', 'H6']
 export function useFormatBlock() {
   const {performCommandWithValue} = useDocumentExecCommand('formatBlock')
 
   return {
     formatBlock(value) {
-      if (value === document.queryCommandValue('formatBlock')) {
-        if (!!window.chrome && isInHTMLList() && document.queryCommandValue('formatBlock').includes('h')) {
+      const blockElValue = document.queryCommandValue('formatBlock')
+      if (value === blockElValue || bothAreHeaderTags(value, blockElValue)) {
+        if (!!window.chrome && isInHTMLList() && blockElValue.includes('h')) {
           removeHeader()
         }
         else {
@@ -26,15 +28,20 @@ function isInHTMLList() {
 function removeHeader() {
   let nodeToBeRemoved = window.getSelection().getRangeAt(0).commonAncestorContainer
   let index = 0
-  while (!['H1', 'H2', 'H3', 'H4', 'H5', 'H6'].includes(nodeToBeRemoved.tagName) && index < 5) {
+  while (!HEADER_TAGS.includes(nodeToBeRemoved.tagName) && index < 5) {
     nodeToBeRemoved = nodeToBeRemoved.parentNode
     index ++
   }
   if (index === 4) { // no header found, contradicting line 9 qcv check, hopefully never happens
-    performCommandWithValue('div')
+    throw new Error(`The element does not appear to be wrapped by a header`)
   }
+  // TODO: replaceNode(nodeToBeRemoved)
   while (nodeToBeRemoved.firstChild) {
     nodeToBeRemoved.parentNode.insertBefore(nodeToBeRemoved.firstChild, nodeToBeRemoved);
   }
   nodeToBeRemoved.parentNode.removeChild(nodeToBeRemoved);
+}
+
+function bothAreHeaderTags (value, elValue) {
+  return HEADER_TAGS.includes(value.toUpperCase()) && HEADER_TAGS.includes(elValue.toUpperCase())
 }
